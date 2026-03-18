0ebe6c47ddc9f6437bd1df779b5e29ed418f204c
ishaan-coder-afk <ishaan1123009@gmail.com>
2026-03-18 11:02:43 +0530

    UC2: print Hello World message

--- HelloApp.java
+++ HelloApp.java
@@ -1,5 +1,16 @@
 public class HelloApp {
-    public static void main(String[] args){
-        System.out.println("Hello World");
+
+    public static void main(String[] args) {
+
+        // Check if user provided a name as command-line argument
+        if (args.length > 0) {
+            String name = args[0];
+            System.out.println("Hello, " + name + "!");
+        }
+        else {
+            // Default message if no argument is given
+            System.out.println("Hello, World!");
+        }
+
     }
 }
